2bd90ca99f5c18d31e0aa38c02e553188a6e149e
VRushLP <rmfarc@uw.edu>
2016-01-30 20:57:29 -0800

    Updating githubio

---
+++ src/main.js
@@ -0,0 +1,66 @@
+var RM_GLOBALS = {
+    
+    FOREST_STAGE : {
+        SCROLL_SPEED: 10000,
+        KNIGHT_SPAWN_X: 0,
+        KNIGHT_SPAWN_Y: 800,
+    },
+}
+
+var AM = new AssetManager();
+
+AM.queueDownload("./img/knight/knight jump.png");
+AM.queueDownload("./img/knight/knight run.png");
+AM.queueDownload("./img/knight/knight standing.png");
+AM.queueDownload("./img/knight/knight jump flipped.png");
+AM.queueDownload("./img/knight/knight run flipped.png");
+AM.queueDownload("./img/knight/knight standing flipped.png");
+AM.queueDownload("./img/forest-stage/forest sky.png");
+AM.queueDownload("./img/forest-stage/forest trees.png");
+AM.queueDownload("./img/forest-stage/forest block.png");
+
+AM.queueStageDownload("./txt/forest-stage.txt");
+
+AM.downloadAll(function () {
+    var canvas = document.getElementById("gameWorld");
+    var ctx = canvas.getContext("2d");
+
+    var gameEngine = new GameEngine();
+    gameEngine.init(ctx);
+    
+    var knight = new Knight(gameEngine, AM, RM_GLOBALS.FOREST_STAGE.KNIGHT_SPAWN_X, RM_GLOBALS.FOREST_STAGE.KNIGHT_SPAWN_Y);
+    knight.entity.controllable = true;
+    knight.entity.moveable = true;
+    knight.entity.fallable = true;
+    knight.entity.camerable = true;
+    knight.entity.respawnable = true;
+    
+    var forestStage = new Stage(ctx, gameEngine, 0, 350);
+    forestStage.addBackground(AM.getAsset("./img/forest-stage/forest sky.png"), 0);
+    forestStage.addBackground(AM.getAsset("./img/forest-stage/forest trees.png"), RM_GLOBALS.FOREST_STAGE.SCROLL_SPEED);
+    forestStage.entityList.push(knight);
+    
+    forestStage.parseLevelFile(AM.getAsset("./txt/forest-stage.txt").split("\n"), AM);    
+    gameEngine.addStage(forestStage);
+
+    var forestBGM = new Howl({
+        urls: ['./snd/bloody_tears.mp3'],
+        volume: .1,
+        loop: true
+    });
+
+    var awake = new Howl({
+        urls: ['./snd/awake.mp3'],
+        volume: .1,
+        loop: true,
+        onend: function () {
+            awake.pos(17.925);
+        }
+    });
+
+    //awake.play();
+    forestBGM.play();
+    gameEngine.start();
+    
+    gameEngine.start();
+});
